c1cec266580a436079d7f08866a5304f1b31d5b3
sharmaut <40994950+sharmaut@users.noreply.github.com>
2020-04-20 13:45:20 +1000

    Add files via upload

--- tempwebhook.ino
+++ tempwebhook.ino
@@ -15,11 +15,14 @@ void setup() {
 
 void loop() {
     analogValue = analogRead(photoresistor);
+    String temp = String(analogValue);
     
     if (analogValue > 15) {
+        Particle.publish("temp", temp, PRIVATE);
         digitalWrite(led1, LOW);
         digitalWrite(led2, LOW);
     } else {
+        Particle.publish("temp", temp, PRIVATE);
         digitalWrite(led1, HIGH);
         digitalWrite(led2, HIGH);
     }
