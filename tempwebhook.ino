8b923a417cb6f0725deccfc5ab73958dcdbc3d80
sharmaut <40994950+sharmaut@users.noreply.github.com>
2020-04-29 22:51:47 +1000

    Add files via upload

--- tempwebhook.ino
+++ tempwebhook.ino
@@ -1,5 +1,5 @@
-int led1 = A5;
-int led2 = D7;  
+int led1 = A5;  
+int led2 = D7;
 
 int photoresistor = A0;
 int analogValue;
@@ -7,23 +7,27 @@ int analogValue;
 
 void setup() {
     
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(photoresistor, INPUT);
-  
+    pinMode(led1, OUTPUT);    
+    pinMode(led2, OUTPUT);
+    pinMode(photoresistor, INPUT);
+
 }
 
 void loop() {
     analogValue = analogRead(photoresistor);
-    String temp = String(analogValue);
+    String brightness = String(analogValue);
     
-    if (analogValue > 15) {
-        Particle.publish("temp", temp, PRIVATE);
+    if (analogValue < 15) {
+       
         digitalWrite(led1, LOW);
         digitalWrite(led2, LOW);
+        Particle.publish("brightness", brightness, PRIVATE);
     } else {
-        Particle.publish("temp", temp, PRIVATE);
+        
         digitalWrite(led1, HIGH);
         digitalWrite(led2, HIGH);
+        Particle.publish("brightness", brightness, PRIVATE);
     }
+    
+    delay(30000);
 }
